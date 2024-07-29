7111eee3cedc560872cf1d6af7681e93df708e53
abhishek pandey <abhishekpy55@gmail.com>
2024-07-29 09:15:01 +0530

    page model framework automation selenium

--- src/test/java/com/thetestingacademy/TestVWOLogin_POM.java
+++ src/test/java/com/thetestingacademy/TestVWOLogin_POM.java
@@ -1,7 +1,9 @@
 package com.thetestingacademy;
 
 import com.thetestingacademy.pages.LoginPage_POM;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -25,6 +27,15 @@ public class TestVWOLogin_POM  {
         LoginPage_POM loginPage_pom = new LoginPage_POM(driver);
         String error_msg_text = loginPage_pom.loginToVWOInvalidCreds("abhishek123@aarrsol.com","admin" );
         Assert.assertEquals(error_msg_text, "Your email, password, IP address or location did not match");
+
+    }@Test
+    public void testLoginPostiveVWO03()  {
+        WebDriver driver = new EdgeDriver();
+        LoginPage_POM loginPage_pom = new LoginPage_POM(driver);
+        String error_msg_text = loginPage_pom.loginToVWOInvalidCreds("contact+atb7x@thetestingacademy.com","Wingify@1234" );
+        WebElement loggedin_username = driver.findElement(By.cssSelector("[data-qa='lufexuloga']"));
+        System.out.println("Logged in user details --> " + loggedin_username.getText());
+        Assert.assertTrue(loggedin_username.getText().contains("Aman Ji"));
         driver.quit();
 
 
